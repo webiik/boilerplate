15fa962b8cbf02753066e9d413a90af34701da5a
Jiri Mihal <jiri@mihal.me>
2017-09-27 17:28:40 +0200

    Added error message.

--- private/app/translations/social-facebook.en.php
+++ private/app/translations/social-facebook.en.php
@@ -3,6 +3,7 @@ return [
     'auth' => [
         'msg' => [
             'err-access-token' => 'Can\'t obtain access token.',
+            'err-app-access-token' => 'Can\'t obtain application access token.',
             'err-user-id' => 'Can\'t obtain user id.',
             'err-email' => 'Can\'t obtain user email.',
         ],
